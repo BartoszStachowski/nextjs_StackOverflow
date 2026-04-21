a1ab365fe87f1072de056e3378869e4096bba8aa
Bartosz Stachowski <bartosz.stachowski@billennium.com>
2026-04-21 15:47:23 +0200

    email and password auth

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,7 +1,91 @@
-import React from "react";
+"use client";
 
-const SignIn = () => {
-  return <div>sign-in</div>;
+import React from "react";
+import { z } from "zod";
+import { Controller, useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { signInSchema } from "@/app/schemas/auth";
+import {
+  Field,
+  FieldError,
+  FieldLabel,
+  FieldGroup,
+} from "@/components/ui/field";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import ROUTES from "@/constants/routes";
+
+const SignInPage = () => {
+  const form = useForm({
+    resolver: zodResolver(signInSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  const onSubmit = (data: z.infer<typeof signInSchema>) => {
+    console.log(data);
+  };
+
+  return (
+    <form onSubmit={form.handleSubmit(onSubmit)} className="mt-10 space-y-6">
+      <FieldGroup className="gap-y-4">
+        <Controller
+          name="email"
+          control={form.control}
+          render={({ field, fieldState }) => (
+            <Field>
+              <FieldLabel>Email Address</FieldLabel>
+              <Input
+                aria-invalid={fieldState.invalid}
+                placeholder="john@doe.com"
+                type="email"
+                autoComplete="email"
+                {...field}
+              />
+              {fieldState.invalid && <FieldError errors={[fieldState.error]} />}
+            </Field>
+          )}
+        />
+
+        <Controller
+          name="password"
+          control={form.control}
+          render={({ field, fieldState }) => (
+            <Field>
+              <FieldLabel>Password</FieldLabel>
+              <Input
+                aria-invalid={fieldState.invalid}
+                placeholder="******"
+                type="password"
+                autoComplete="current-password"
+                {...field}
+              />
+              {fieldState.invalid && <FieldError errors={[fieldState.error]} />}
+            </Field>
+          )}
+        />
+        <Button
+          className="primary-gradient paragraph-medium min-h-12 w-full
+            rounded-md px-4 py-3 font-inter text-light-900"
+        >
+          Sign in
+        </Button>
+
+        <p>
+          Don&apos;t have an account?{" "}
+          <Link
+            className="paragraph-semibold primary-text-gradient"
+            href={ROUTES.SIGN_UP}
+          >
+            Sign up
+          </Link>
+        </p>
+      </FieldGroup>
+    </form>
+  );
 };
 
-export default SignIn;
+export default SignInPage;
--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,7 +1,117 @@
-import React from "react";
+"use client";
 
-const SignUp = () => {
-  return <div>sign-up</div>;
+import React from "react";
+import {
+  Field,
+  FieldError,
+  FieldGroup,
+  FieldLabel,
+} from "@/components/ui/field";
+import { Controller, useForm } from "react-hook-form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import ROUTES from "@/constants/routes";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { signUpSchema } from "@/app/schemas/auth";
+import { z } from "zod";
+
+const SignUpPage = () => {
+  const form = useForm({
+    resolver: zodResolver(signUpSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+      name: "",
+      username: "",
+    },
+  });
+
+  const onSubmit = (data: z.infer<typeof signUpSchema>) => {
+    console.log(data);
+  };
+
+  return (
+    <form onSubmit={form.handleSubmit(onSubmit)} className="mt-10 space-y-6">
+      <FieldGroup className="gap-y-4">
+        <Controller
+          name="email"
+          control={form.control}
+          render={({ field, fieldState }) => (
+            <Field>
+              <FieldLabel>Email Address</FieldLabel>
+              <Input
+                aria-invalid={fieldState.invalid}
+                placeholder="john@doe.com"
+                type="email"
+                autoComplete="email"
+                {...field}
+              />
+              {fieldState.invalid && <FieldError errors={[fieldState.error]} />}
+            </Field>
+          )}
+        />
+
+        <Controller
+          name="password"
+          control={form.control}
+          render={({ field, fieldState }) => (
+            <Field>
+              <FieldLabel>Password</FieldLabel>
+              <Input
+                aria-invalid={fieldState.invalid}
+                placeholder="******"
+                type="password"
+                autoComplete="current-password"
+                {...field}
+              />
+              {fieldState.invalid && <FieldError errors={[fieldState.error]} />}
+            </Field>
+          )}
+        />
+
+        <Controller
+          name="name"
+          control={form.control}
+          render={({ field, fieldState }) => (
+            <Field>
+              <FieldLabel>Name</FieldLabel>
+              <Input aria-invalid={fieldState.invalid} {...field} />
+              {fieldState.invalid && <FieldError errors={[fieldState.error]} />}
+            </Field>
+          )}
+        />
+
+        <Controller
+          name="username"
+          control={form.control}
+          render={({ field, fieldState }) => (
+            <Field>
+              <FieldLabel>Username</FieldLabel>
+              <Input aria-invalid={fieldState.invalid} {...field} />
+              {fieldState.invalid && <FieldError errors={[fieldState.error]} />}
+            </Field>
+          )}
+        />
+        <Button
+          className="primary-gradient paragraph-medium min-h-12 w-full
+            rounded-md px-4 py-3 font-inter text-light-900"
+        >
+          Sign in
+        </Button>
+
+        <p>
+          Already have an account?{" "}
+          <Link
+            className="paragraph-semibold primary-text-gradient"
+            href={ROUTES.SIGN_IN}
+          >
+            Sign In
+          </Link>
+        </p>
+      </FieldGroup>
+    </form>
+  );
 };
 
-export default SignUp;
+export default SignUpPage;
