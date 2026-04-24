1b25cf0d10fd5bc07ad1ebf728fd7d7be9204195
Bartosz Stachowski <bartosz.stachowski@billennium.com>
2026-04-24 12:56:57 +0200

    add LocalSearch to homepage

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,8 +2,65 @@ import React from "react";
 import { Button } from "@/components/ui/button";
 import ROUTES from "@/constants/routes";
 import Link from "next/link";
+import LocalSearch from "@/components/web/search/LocalSearch";
 
-const Page = async () => {
+const questions = [
+  {
+    _id: "1",
+    title: "How to learn React?",
+    description: "I want to learn React, can anyone help me?",
+    tags: [
+      { _id: "1", name: "React " },
+      { _id: "2", name: "JavaScript" },
+    ],
+    author: { _id: "1", name: "John Kowalsky" },
+    upvotes: 10,
+    answers: 5,
+    views: 100,
+    createdAt: new Date(),
+  },
+  {
+    _id: "2",
+    title: "Best practices for Node.js API?",
+    description:
+      "What are the industry standards for structuring a Node.js API?",
+    tags: [
+      { _id: "3", name: "Node.js" },
+      { _id: "4", name: "Backend" },
+    ],
+    author: { _id: "2", name: "Jane Smith" },
+    upvotes: 25,
+    answers: 3,
+    views: 150,
+    createdAt: new Date(),
+  },
+  {
+    _id: "3",
+    title: "Flexbox vs Grid?",
+    description: "When should I use CSS Grid and when is Flexbox better?",
+    tags: [
+      { _id: "5", name: "CSS" },
+      { _id: "6", name: "Frontend" },
+    ],
+    author: { _id: "3", name: "Marek Nowak" },
+    upvotes: 40,
+    answers: 8,
+    views: 300,
+    createdAt: new Date(),
+  },
+];
+
+interface SearchParams {
+  searchParams: Promise<{ [key: string]: string }>;
+}
+
+const HomePage = async ({ searchParams }: SearchParams) => {
+  const { query = "" } = await searchParams;
+
+  const filteredQuestions = questions.filter((question) =>
+    question.title.toLowerCase().includes(query?.toLowerCase())
+  );
+
   return (
     <>
       <section
@@ -18,16 +75,22 @@ const Page = async () => {
           <Link href={ROUTES.ASK_QUESTION}>Ask a Question</Link>
         </Button>
       </section>
-      <section className="mt-11">LocalSearch</section>
-      HomeFilter
+      <section className="mt-11">
+        <LocalSearch
+          route="/"
+          imgSrc="/icons/search.svg"
+          placeholder="Search questions..."
+          otherClasses="flex-1"
+        />
+      </section>
+      {/*HomeFilter*/}
       <div className="mt-10 flex w-full flex-col gap-6">
-        <p>Question Card 1</p>
-        <p>Question Card 1</p>
-        <p>Question Card 1</p>
-        <p>Question Card 1</p>
+        {filteredQuestions.map((question) => (
+          <h1 key={question._id}>{question.title}</h1>
+        ))}
       </div>
     </>
   );
 };
 
-export default Page;
+export default HomePage;
